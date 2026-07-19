3bb357d9bcd5ec46bd6cd5cd9e900a7005b29e7f
Quang Huy <huy.tnq@gmail.com>
2026-07-19 16:14:52 +0700

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './AboutUs.css'; // Đảm bảo bạn có file CSS này để trang trí
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h1 className="about-us-heading">About Us</h1>
+      <p className="about-us-description">
+        Welcome to Paradise Nursery! We are dedicated to providing the highest quality plants 
+        and gardening supplies to help you create your own green oasis at home. 
+        Our mission is to bring nature closer to your everyday life.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
